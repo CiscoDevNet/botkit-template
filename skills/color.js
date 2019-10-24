9740fa25b7763dca80676503722602bb29467727
jianbfan <jianbfan@jianbfans-MacBook-Pro.local>
2019-10-23 22:47:17 -0700

    Change to asksummer

--- skills/color.js
+++ skills/color.js
@@ -6,7 +6,7 @@ module.exports = function (controller) {
     controller.hears([/^color$/], 'direct_message,direct_mention', function (bot, message) {
 
         bot.startConversation(message, function (err, convo) {
-            convo.say('This is a Summer Botkit conversation sample.');
+            convo.say('This is a AskSummer Bot.');
 
             convo.ask('What is your favorite color?', function (response, convo) {
                 convo.say("Cool, I like '" + response.text + "' too!");
